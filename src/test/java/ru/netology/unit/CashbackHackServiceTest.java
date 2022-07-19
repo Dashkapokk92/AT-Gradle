f969cd0068fba9e7ee8b74579ecffa0d96bf71b5
Danem <Danem22mail.ru>
2022-07-19 13:21:22 +0300

    initial commit junit4

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -1,33 +1,33 @@
 package ru.netology.unit;
-import org.testng.annotations.Test;
+import org.junit.Test;
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
-  CashbackHackService service = new CashbackHackService();
+    CashbackHackService service = new CashbackHackService();
 
     @Test
     public void shouldCalculateTheAmount() {
         int amount = 3300;
         int actual = service.remain(amount);
         int expected = 700;
-
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
     @Test
     public void shouldCalculateTheAmountIfZero() {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
+
     @Test
     public void shouldCalculateTheAmountAndAdd1() {
         int amount = 999;
         int actual = service.remain(amount);
         int expected = 1;
-
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -35,7 +35,6 @@ class CashbackHackServiceTest {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
